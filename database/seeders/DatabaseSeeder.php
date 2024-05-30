279be079ce14d5f39c4b271b115a05a138e1e5fd
daniel <daniel@smartm.com.tw>
2024-05-30 20:05:59 +0800

    新增設定table 調整HandleExecuteScheduleCommand.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Settings\Entities\SettingEntity;
 use App\Models\Users\Entities\UserEntity;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -32,6 +33,15 @@ class DatabaseSeeder extends Seeder
             'influx_db_connection' => config('influxdb.host'),
             'influx_db_bucket' => config('influxdb.bucket'),
             'influx_db_token' => config('influxdb.token'),
+        ]);
+
+        app(SettingEntity::class)->create([
+            'id' => 1,
+            'delay_minutes' => 5,
+            'schedule_check_interval_minutes' => 15,
+            'task_completion_alert_threshold_minutes' => 15,
+            'download_task_alert_threshold_minutes' => 30,
+            'domain_list_chuck' => 500,
         ]);
     }
 }
