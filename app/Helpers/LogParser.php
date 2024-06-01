b25f6e019af24daaff4a88abe3a0c2b8789f96f4
Roy <cc711612@gmail.com>
2024-06-01 23:05:18 +0800

    fix log pattern

--- app/Helpers/LogParser.php
+++ app/Helpers/LogParser.php
@@ -15,38 +15,66 @@ class LogParser
     {
         switch ($serviceType) {
             case '1115':
-                $pattern = '/^(\S+) (\S+) (\S+) \[([^]]+)\] "(\S+) (\S+) (\S+)" (\d+) (\d+) (\S+) (\S+) (\S+) (\S+) "([^"]*)" "([^"]*)"$/';
-                
-                break;
+                // %host %uident %uname [%rt] "%method %url %rp" %code %size "%referer" "%ua"
+                $pattern = '/^(\d+\.\d+\.\d+\.\d+) ([^ ]+) ([^ ]+) \[([^\]]+)\] "(GET|POST|PUT|DELETE|OPTIONS|HEAD|PATCH) ([^ ]+) HTTP\/(\d+\.\d+)" (\d+) (\d+) "([^"]*)" "([^"]*)"/';
+                preg_match($pattern, $logEntry, $matches);
+                return [
+                    'host' => $matches[1],
+                    'uident' => $matches[2],
+                    'uname' => $matches[3],
+                    'rt' => $matches[4],
+                    'method' => $matches[5],
+                    'url' => $matches[6],
+                    'http_version' => $matches[7],
+                    'code' => $matches[8],
+                    'size' => $matches[9],
+                    'referer' => $matches[10],
+                    'ua' => $matches[11],
+                ];
             case '1028':
+                // %host %uident %uname [%rt] "%method %url %rp" %code %size %cache %pic_bhs %pic_bt %tru "%referer" "%ua"
                 $pattern = '/^(\S+) (\S+) (\S+) \[([^]]+)\] "(\S+) (\S+) (\S+)" (\d+) (\d+) (\S+) (\S+) (\S+) (\S+) "([^"]*)" "([^"]*)"$/';
-
-                break;
+                preg_match($pattern, $logEntry, $matches);
+                return [
+                    'host' => $matches[1],
+                    'uident' => $matches[2],
+                    'uname' => $matches[3],
+                    'rt' => $matches[4],
+                    'method' => $matches[5],
+                    'url' => $matches[6],
+                    'http_version' => $matches[7],
+                    'code' => $matches[8],
+                    'size' => $matches[9],
+                    'cache' => $matches[10],
+                    'pic_bhs' => $matches[11],
+                    'pic_bt' => $matches[12],
+                    'tru' => $matches[13],
+                    'referer' => $matches[14],
+                    'ua' => $matches[15],
+                ];
             case '1551':
+                // %host %uident %uname [%rt] "%method %url %rp" %code %size "%referer" "%ua" "%aty" "%ra" "%Content-Type"
                 $pattern = '/(\S+) (\S+) (\S+) \[(.+?)\] "(\S+) (\S+) (\S+)" (\d+) (\d+) "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)"/';
+                preg_match($pattern, $logEntry, $matches);
+                return [
+                    'host' => $matches[1],
+                    'uident' => $matches[2],
+                    'uname' => $matches[3],
+                    'rt' => $matches[4],
+                    'method' => $matches[5],
+                    'url' => $matches[6],
+                    'http_version' => $matches[7],
+                    'code' => $matches[8],
+                    'size' => $matches[9],
+                    'referer' => $matches[10],
+                    'ua' => $matches[11],
+                    'aty' => $matches[12],
+                    'ra' => $matches[13],
+                    'Content-Type' => $matches[14],
+                ];
 
-                break;
             default:
                 throw new \Exception('Unknown service type');
         }
-
-        preg_match($pattern, $logEntry, $matches);
-
-        return [
-            'host' => $matches[1],                    // 主機
-            'uident' => $matches[2],                  // 使用者標識
-            'uname' => $matches[3],                   // 使用者名稱
-            'rt' => $matches[4],                      // 請求時間
-            'method' => $matches[5],                  // 請求方法
-            'url' => $matches[6],                     // URL
-            'rp' => $matches[7],                      // 路徑
-            'code' => $matches[8],                    // 響應狀態碼
-            'size' => $matches[9],                    // 響應大小
-            'referer' => $matches[10],                // 參考網址
-            'ua' => $matches[11],                     // 用戶代理
-            'aty' => $matches[12] ?? null,            // 攻擊類型
-            'ra' => $matches[13] ?? null,             // 保護操作
-            'Content-Type' => $matches[14] ?? null   // 內容類型
-        ];
     }
 }
