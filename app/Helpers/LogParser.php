93a42e0469567815212a60e2271663d04266eb9c
Roy <cc711612@gmail.com>
2024-06-02 21:56:31 +0800

    修正log parse

--- app/Helpers/LogParser.php
+++ app/Helpers/LogParser.php
@@ -2,6 +2,8 @@
 
 namespace App\Helpers;
 
+use Illuminate\Support\Facades\Log;
+
 class LogParser
 {
     /**
@@ -13,68 +15,73 @@ class LogParser
      */
     public function parseLogEntry($logEntry, $serviceType = null)
     {
-        switch ($serviceType) {
-            case '1115':
-                // %host %uident %uname [%rt] "%method %url %rp" %code %size "%referer" "%ua"
-                $pattern = '/^(\d+\.\d+\.\d+\.\d+) ([^ ]+) ([^ ]+) \[([^\]]+)\] "(GET|POST|PUT|DELETE|OPTIONS|HEAD|PATCH) ([^ ]+) HTTP\/(\d+\.\d+)" (\d+) (\d+) "([^"]*)" "([^"]*)"/';
-                preg_match($pattern, $logEntry, $matches);
-                return [
-                    'host' => $matches[1],
-                    'uident' => $matches[2],
-                    'uname' => $matches[3],
-                    'rt' => $matches[4],
-                    'method' => $matches[5],
-                    'url' => $matches[6],
-                    'http_version' => $matches[7],
-                    'code' => $matches[8],
-                    'size' => $matches[9],
-                    'referer' => $matches[10],
-                    'ua' => $matches[11],
-                ];
-            case '1028':
-                // %host %uident %uname [%rt] "%method %url %rp" %code %size %cache %pic_bhs %pic_bt %tru "%referer" "%ua"
-                $pattern = '/^(\S+) (\S+) (\S+) \[([^]]+)\] "(\S+) (\S+) (\S+)" (\d+) (\d+) (\S+) (\S+) (\S+) (\S+) "([^"]*)" "([^"]*)"$/';
-                preg_match($pattern, $logEntry, $matches);
-                return [
-                    'host' => $matches[1],
-                    'uident' => $matches[2],
-                    'uname' => $matches[3],
-                    'rt' => $matches[4],
-                    'method' => $matches[5],
-                    'url' => $matches[6],
-                    'http_version' => $matches[7],
-                    'code' => $matches[8],
-                    'size' => $matches[9],
-                    'cache' => $matches[10],
-                    'pic_bhs' => $matches[11],
-                    'pic_bt' => $matches[12],
-                    'tru' => $matches[13],
-                    'referer' => $matches[14],
-                    'ua' => $matches[15],
-                ];
-            case '1551':
-                // %host %uident %uname [%rt] "%method %url %rp" %code %size "%referer" "%ua" "%aty" "%ra" "%Content-Type"
-                $pattern = '/(\S+) (\S+) (\S+) \[(.+?)\] "(\S+) (\S+) (\S+)" (\d+) (\d+) "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)"/';
-                preg_match($pattern, $logEntry, $matches);
-                return [
-                    'host' => $matches[1],
-                    'uident' => $matches[2],
-                    'uname' => $matches[3],
-                    'rt' => $matches[4],
-                    'method' => $matches[5],
-                    'url' => $matches[6],
-                    'http_version' => $matches[7],
-                    'code' => $matches[8],
-                    'size' => $matches[9],
-                    'referer' => $matches[10],
-                    'ua' => $matches[11],
-                    'aty' => $matches[12],
-                    'ra' => $matches[13],
-                    'Content-Type' => $matches[14],
-                ];
+        $logEntry = trim($logEntry);
+        try {
+            switch ($serviceType) {
+                case '1115':
+                    // %host %uident %uname [%rt] "%method %url %rp" %code %size "%referer" "%ua"
+                    $pattern = '/^(?P<host>\d+\.\d+\.\d+\.\d+) (?P<uident>[^ ]+) (?P<uname>[^ ]+) \[(?P<rt>[^\]]+)\] "(?P<method>\S+) (?P<url>\S+) (?P<http_version>[^"]+)" (?P<code>\d+) (?P<size>\d+) (?P<cache_status>\S+) (?P<http_status>\d+) (?P<unknown>\d+) - "(?P<referer>[^"]*)" "(?P<ua>[^"]+)"$/';
+                    preg_match($pattern, $logEntry, $matches);
+                    return [
+                        'host' => $matches['host'],
+                        'uident' => $matches['uident'],
+                        'uname' => $matches['uname'],
+                        'rt' => $matches['rt'],
+                        'method' => $matches['method'],
+                        'url' => $matches['url'],
+                        'http_version' => $matches['http_version'],
+                        'code' => $matches['code'],
+                        'size' => $matches['size'],
+                        'referer' => $matches['referer'],
+                        'ua' => $matches['ua'],
+                    ];
+                case '1028':
+                    // %host %uident %uname [%rt] "%method %url %rp" %code %size %cache %pic_bhs %pic_bt %tru "%referer" "%ua"
+                    $pattern = '/^(\S+) (\S+) (\S+) \[([^]]+)\] "(\S+) (\S+) (\S+)" (\d+) (\d+) (\S+) (\S+) (\S+) (\S+) "([^"]*)" "([^"]*)"$/';
+                    preg_match($pattern, $logEntry, $matches);
+                    return [
+                        'host' => $matches[1],
+                        'uident' => $matches[2],
+                        'uname' => $matches[3],
+                        'rt' => $matches[4],
+                        'method' => $matches[5],
+                        'url' => $matches[6],
+                        'http_version' => $matches[7],
+                        'code' => $matches[8],
+                        'size' => $matches[9],
+                        'cache' => $matches[10],
+                        'pic_bhs' => $matches[11],
+                        'pic_bt' => $matches[12],
+                        'tru' => $matches[13],
+                        'referer' => $matches[14],
+                        'ua' => $matches[15],
+                    ];
+                case '1551':
+                    // %host %uident %uname [%rt] "%method %url %rp" %code %size "%referer" "%ua" "%aty" "%ra" "%Content-Type"
+                    $pattern = '/(\S+) (\S+) (\S+) \[(.+?)\] "(\S+) (\S+) (\S+)" (\d+) (\d+) "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)"/';
+                    preg_match($pattern, $logEntry, $matches);
+                    return [
+                        'host' => $matches[1],
+                        'uident' => $matches[2],
+                        'uname' => $matches[3],
+                        'rt' => $matches[4],
+                        'method' => $matches[5],
+                        'url' => $matches[6],
+                        'http_version' => $matches[7],
+                        'code' => $matches[8],
+                        'size' => $matches[9],
+                        'referer' => $matches[10],
+                        'ua' => $matches[11],
+                        'aty' => $matches[12],
+                        'ra' => $matches[13],
+                        'Content-Type' => $matches[14],
+                    ];
 
-            default:
-                throw new \Exception('Unknown service type');
+                default:
+                    throw new \Exception('Unknown service type');
+            }
+        } catch (\Exception $e) {
+            Log::driver('log_parse')->info('type' . $serviceType . ' logEntry:' . $logEntry);
         }
     }
 }
