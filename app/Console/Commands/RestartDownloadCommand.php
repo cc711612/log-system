45ce3dca80831c7eac552d7aa82518aeaca271e4
Roy <cc711612@gmail.com>
2024-06-18 11:21:38 +0800

    add restart download command

---
+++ app/Console/Commands/RestartDownloadCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Helpers\Enums\StatusEnum;
+use App\Jobs\HandleDownloadJob;
+use App\Models\Downloads\Entities\DownloadEntity;
+use Illuminate\Console\Command;
+
+class RestartDownloadCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:restart_download {--downloadIds= : downloadIds}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '重新啟動 download jobs';
+
+    public function handle()
+    {
+        if ($this->option('downloadIds')) {
+            $downloadIds = explode(',', $this->option('downloadIds'));
+            $downloadIds = $this->getDownloadIdsByIds($downloadIds);
+        } else {
+            $downloadIds = $this->getDownloadIds();
+        }
+        foreach ($downloadIds as $downloadId) {
+            HandleDownloadJob::dispatch($downloadId);
+        }
+        $this->info('已重新啟動 download jobs : ' . implode(',', $downloadIds));
+    }
+
+    private function getDownloadIds()
+    {
+        $status = [
+            StatusEnum::PROCESSING->value,
+            StatusEnum::FAILURE->value
+        ];
+
+        return DownloadEntity::whereIn('status', $status)
+            ->select(['id'])
+            ->get()
+            ->pluck('id')
+            ->toArray();
+    }
+
+    private function getDownloadIdsByIds(array $downloadIds)
+    {
+        return DownloadEntity::whereIn('id', $downloadIds)
+            ->select(['id'])
+            ->get()
+            ->pluck('id')
+            ->toArray();
+    }
+}
